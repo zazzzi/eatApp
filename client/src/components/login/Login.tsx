4ec8bf1a16ed15ea402a0aaf1576364252830379
SebastianZazzi <sebastian@cetrez.com>
2021-12-30 15:47:14 +0100

    implemented form

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,20 +1,40 @@
-import { Box, makeStyles, Theme} from "@material-ui/core";
+import { Box, makeStyles, Theme, Typography} from "@material-ui/core";
 import { useEffect, useState } from "react";
-
+import eatAppLogo from "../../assets/logos/eatAppLogo.png"
+import LoginInputForm from "./LoginInputForm";
 interface Iprops {
  
 }
 
 function Login(props: Iprops) {
-
+const classes = useStyles();
   return (
    <Box>
-       TEST
+     <Box>
+       <img className={classes.logo} src={eatAppLogo} alt="eatAppLogo.png"/>
+     </Box>
+     <Box>
+       <Typography variant="h2">
+         Välkommen
+       </Typography>
+       <Typography variant="body2">
+        Logga in på ditt konto här.
+       </Typography>
+     </Box>
+     <Box>
+       <LoginInputForm/>
+     </Box>
    </Box>
   );
 }
 
-const useStyles = makeStyles((theme: Theme) => ({}));
+const useStyles = makeStyles((theme: Theme) => ({
+  logo: {
+    display: "flex",
+    width: "100vw",
+    marginTop: "100px",
+  },
+}));
 
 
 export default Login; 
