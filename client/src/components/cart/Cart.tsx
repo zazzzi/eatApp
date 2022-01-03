cc095542ad2503475fe7f13297fe6550516732cc
Alexander Liljedahl <alexander@alexwho.se>
2022-01-03 15:36:20 +0100

    imstuuuuckkk

--- client/src/components/cart/Cart.tsx
+++ client/src/components/cart/Cart.tsx
@@ -19,7 +19,7 @@ function Cart(props: Iprops) {
      <Typography variant="h4" gutterBottom component="div">
         Cart
       </Typography>
-      <MenuItem menuItem = {food}/>
+      <MenuItem menuItem = {food} category={"all"} />
       <Divider />
       <Box className={classes.align}>
         <Box className={classes.priceTotal}>
--- client/src/food.tsx
+++ client/src/food.tsx
@@ -1,20 +1,20 @@
 const food = [
 {
   id: "1234",
-  title: "Food",
-  price: 123,
+  title: "Stor Stark",
+  price: 69,
   quantity: 1,
-  content: ["12,6%"],
-  category: ["drink", 'food'],
+  content: ["4,6%"],
+  category: ["drink", "all"],
   img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
 },
 {
   id: "1234",
-  title: "Food",
+  title: "Negroni",
   price: 123,
   quantity: 3,
   content: ["2 cl Gin", "2 cl Martini Rosso", "2 cl Campari Bitter"],
-  category: ["drink", 'food'],
+  category: ["drink", "all"],
   img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
 },
 {
@@ -23,7 +23,43 @@ const food = [
   price: 123,
   quantity: 1,
   content: ["12,6%"],
-  category: ["drink", 'food'],
+  category: ['food', "all"],
+  img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
+},
+{
+  id: "1234",
+  title: "Food",
+  price: 123,
+  quantity: 1,
+  content: ["12,6%"],
+  category: ['food', "all"],
+  img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
+},
+{
+  id: "1234",
+  title: "Food",
+  price: 123,
+  quantity: 1,
+  content: ["12,6%"],
+  category: ['food', "all"],
+  img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
+},
+{
+  id: "1234",
+  title: "Food",
+  price: 123,
+  quantity: 1,
+  content: ["12,6%"],
+  category: ['food', "all"],
+  img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
+},
+{
+  id: "1234",
+  title: "Food",
+  price: 123,
+  quantity: 1,
+  content: ["12,6%"],
+  category: ['food', "all"],
   img: "https://produits.bienmanger.com/6105-0w0h0_Maredsous_Tripel_Abbey_Beer_From_Belgium.jpg"
 },
 ]
