8a9387edd93f7e6b6e4d81786e6cc6c24012c4d0
Intradastingly <asaf_lackgren@hotmail.com>
2022-01-03 11:32:52 +0100

    stepper set up

--- client/src/components/checkout/Checkout.tsx
+++ client/src/components/checkout/Checkout.tsx
@@ -1,163 +1,107 @@
+import React from "react";
 import { 
   Box, 
   makeStyles, 
   Theme, 
-  Stepper, 
-  Step, 
-  StepLabel, 
   Button, 
-  Typography
+  Typography,
+  MobileStepper,
+  useTheme,
 } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import PaymentMethod from "./PaymentMethod"
 import CardPayment from "./CardPayment"
 import SwishPayment from "./Swish"
 import OrderConfirmation from "./OrderConfirmation"
-import React from "react";
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 
 
 interface Iprops {
  
 }
 
-const steps = ['Betala', 'Betalning', 'Klart'];
-
-function Checkout(props: Iprops) {
-  const [activeStep, setActiveStep] = useState(0);
-  const [skipped, setSkipped] = useState(new Set<number>());
+function Checkout() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
   const classes = useStyles();
 
-  const isStepOptional = (step: number) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
-
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
   };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
+  const getStepContent = (stepIndex: number) => {
+    switch(stepIndex){
+      case 0:
+        return(
+          <Box>
+            <PaymentMethod/>
+          </Box>
+        );
+      case 1: 
+      //change this depending on payment method chosen
+        return(
+        <Box>
+          <SwishPayment/>
+        </Box>
+      )
+      case 2: 
+        return (
+          <Box>
+            <OrderConfirmation/>
+          </Box>
+        )
+      default:
+        return "Unknown stepIndex";
     }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+  }
+  
 
   return (
-    <Box className={classes.topContainer}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography className={classes.stepsCompleted}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography className={classes.stepsCompleted}>Step {activeStep + 1}</Typography>
-          <Box className={classes.back}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              className={classes.backButton}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip}>
-                Skip
-              </Button>
+    <Box>
+      <MobileStepper
+        variant="dots"
+        steps={3}
+        position="static"
+        activeStep={activeStep}
+        className={classes.style}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 2}>
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
             )}
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+      <Box>
+        {getStepContent(activeStep)}
+      </Box>
     </Box>
   );
 }
- /*  return (
-   <Box>
-      <PaymentMethod/>
-      <CardPayment/>
-      <SwishPayment/>
-      <OrderConfirmation/>
-   </Box>
-  ); 
-  }*/
-
 
 const useStyles = makeStyles((theme: Theme) => ({
-  topContainer: {
-    width: '100%' 
+  style: {
+    maxWidth: 400, 
+    flexGrow: 1
   }, 
-  stepsCompleted: {
-    mt: 2, 
-    mb: 1
-  }, 
-  back: {
-    display: 'flex', 
-    flexDirection: 'row', 
-    pt: 2 
-  },
-  backButton: {
-    mr: 1 
-  }
-
 }));
-
 
 export default Checkout; 
